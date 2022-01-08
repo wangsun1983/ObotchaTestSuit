0363f2506880e791da13ded80b3935351ea91ccb
sunli.wang <wang_sun_1983@yahoo.co.jp>
2022-01-08 22:19:40 +0800

    update TestLog to support dump param

--- common/TestLog.hpp
+++ common/TestLog.hpp
@@ -1,11 +1,20 @@
 #ifndef __OBOTCHA_TEST_LOG_HPP__
 #define __OBOTCHA_TEST_LOG_HPP__
 
-#define TEST_OK(X) \
-    printf("%-128s :\033[32m%s \033[37m\n",X,"[OK]")
+#define TEST_OK(...) \
+    {\
+        char buff[1024];\
+        memset(buff,0,1024);\
+        sprintf(buff,__VA_ARGS__);\
+        printf("%-128s :\033[32m%s \033[37m\n",buff,"[OK]");\
+    }
 
-#define TEST_FAIL(X) \
-    printf("%-128s :\033[31m%s \033[37m\n",X,"[FAIL]")
-
+#define TEST_FAIL(...) \
+    {\
+        char buff[1024];\
+        memset(buff,0,1024);\
+        sprintf(buff,__VA_ARGS__);\
+        printf("%-128s :\033[31m%s \033[37m\n",buff,"[FAIL]");\
+    }
 
 #endif
