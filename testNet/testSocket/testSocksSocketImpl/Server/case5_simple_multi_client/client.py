b027aa4070ff76701683ae1d8269e808ca95b988
sunli.wang <wang_sun_1983@yahoo.co.jp>
2022-03-14 23:18:43 +0800

    1.add sockt multi thread unit test

---
+++ testNet/testSocket/testSocksSocketImpl/Server/case5_simple_multi_client/client.py
@@ -0,0 +1,61 @@
+import socket
+import time
+import threading
+
+import sys
+sys.path.append(r'../../../../../common')
+from NetPort import getEnvPort
+
+
+class myThread (threading.Thread):
+  def __init__(self,name):
+        threading.Thread.__init__(self)
+        self.name = name
+
+  def run(self):      
+    count = 1024
+    while count > 0:
+      self.s = socket.socket()
+      self.s.connect(("127.0.0.1",getEnvPort()))
+      self.s.send(str("hello world").encode("utf-8"))
+
+
+thread1 = myThread("Thread-1")
+thread1.start()
+thread2 = myThread("Thread-2")
+thread2.start()
+thread3 = myThread("Thread-2")
+thread3.start()
+thread4 = myThread("Thread-2")
+thread4.start()
+thread5 = myThread("Thread-2")
+thread5.start()
+thread6 = myThread("Thread-2")
+thread6.start()
+thread7 = myThread("Thread-2")
+thread7.start()
+thread8 = myThread("Thread-2")
+thread8.start()
+thread9 = myThread("Thread-2")
+thread9.start()
+thread10 = myThread("Thread-2")
+thread10.start()
+thread11 = myThread("Thread-2")
+thread11.start()
+thread12 = myThread("Thread-2")
+thread12.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
+thread5.join()
+thread6.join()
+thread7.join()
+thread8.join()
+thread9.join()
+thread10.join()
+thread11.join()
+thread12.join()
+
+print("test finished")
---
+++ testNet/testSocket/testSocksSocketImpl/Server/case5_simple_multi_client/main.cpp
@@ -0,0 +1,60 @@
+#include "DatagramSocketImpl.hpp"
+#include "SocketBuilder.hpp"
+#include "SocketMonitor.hpp"
+#include "Inet4Address.hpp"
+#include "AutoLock.hpp"
+#include "Condition.hpp"
+#include "TestLog.hpp"
+#include "NetPort.hpp"
+#include "NetEvent.hpp"
+#include "CountDownLatch.hpp"
+
+using namespace obotcha;
+
+String message = createString("");
+bool isFirst = true;
+
+Mutex mMutex = createMutex();
+Condition mCond = createCondition();
+
+CountDownLatch latch = createCountDownLatch(1024*12);
+
+DECLARE_CLASS(MyListener) IMPLEMENTS(SocketListener) {
+
+public:
+  void onSocketMessage(int event,Socket s,ByteArray data) {
+    switch(event) {
+      case st(NetEvent)::Message: {
+        if(data != nullptr && data->toString()->equals("hello world")) {
+          latch->countDown();
+        }
+      }
+    }
+  }
+};
+
+int main() {
+  int port = getEnvPort();
+  InetAddress addr = createInet4Address(port);
+  ServerSocket sock = createSocketBuilder()->setAddress(addr)->newServerSocket();
+  int result = sock->bind();
+  SocketMonitor monitor = createSocketMonitor(4);
+  MyListener l = createMyListener();
+  monitor->bind(sock,l);
+
+  Thread t = createThread([] {
+    while(1) {
+      usleep(1000*1000);
+      printf("count is %d \n",latch->getCount());
+    }
+  });
+  t->start();
+
+  latch->await();
+  monitor->close();
+
+  port++;
+  setEnvPort(port);
+  TEST_OK("Test Tcp Server case5_simple_multi_client test100");
+
+}
